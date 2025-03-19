3dc28f3ae9d0d7028d6d48b7cafa3a5bbd9fad90
rabby-shek <rabbyshek1998@gmail.com>
2025-03-19 23:18:42 +0600

    learning game functionalities

--- script.js
+++ script.js
@@ -1,59 +1,62 @@
-const gameCanvas = document.getElementById('gameCanvas');
+let gameCanvas = document.getElementById('gameCanvas');
 const ctx = gameCanvas.getContext('2d');
-const displayScore = document.getElementById('score');
-const resetBtn = document.getElementById('resetBtn');
-const gameCanvasWidth = gameCanvas.width;
-const gameCanvasHeight = gameCanvas.height;
-const gameCanvasBGColor = "black";
-const snakeColor = "white";
-const snakeBorder = "black";
-const foodColor = "purple";
-const unitSize = 25;
-let running = false;
-let xVelocity = unitSize;
-let yVelocity = 0;
-let foodX;
-let foodY;
-let snake = [
-    { x: unitSize * 1, y: 0 },
-    { x: unitSize * 2, y: 0 },
-    { x: 0, y: 0 },
-];
-gameCanvas.style.backgroundColor = gameCanvasBGColor;
+const gameCanvasBgColor = "black";
+let canvasWidth = gameCanvas.width = 500;
+let canvasHeight = gameCanvas.height = window.innerHeight;
+const snakeBodyColor = 'purple';
+const snakeBorderColor = 'black';
+const foodColor = 'red';
+const uniteSize = 15;
+let foodXPosition = 0;
+let foodYPosition = 0;
 
-window.addEventListener('keydown', changeDirection);
-resetBtn.addEventListener('click', resetGame);
-gameStart();
+const snake = [
+    { x: 150, y: 150 },
+    { x: 135, y: 150 },
+    { x: 120, y: 150 },
+    { x: 105, y: 150 }
+]
 
-function gameStart() {
-    createFood();
-    drawFood();
-    drawSnake();
-}
-function nextTick() { }
-function clearCanvas() { }
-function createFood() {
-    function randomFood(min, max) {
-        const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
-        return randNum;
-    }
-    foodX = randomFood(0, gameCanvasWidth - unitSize);
-    foodY = randomFood(0, gameCanvasWidth - unitSize);
-}
-function drawFood() {
-    ctx.fillStyle = foodColor;
-    ctx.fillRect(foodX, foodY, unitSize, unitSize);
-}
-function moveSnake() { }
+drawSnake();
+drawFood();
 function drawSnake() {
-    ctx.fillStyle = snakeColor;
-    ctx.strokeStyle = snakeBorder;
-    snake.forEach((item) => {
-        ctx.fillRect(item.x, item.y, unitSize, unitSize);
-        ctx.strokeRect(item.x, item.y, unitSize, unitSize);
-    })
+    snake.forEach((unit) => {
+        ctx.fillStyle = snakeBodyColor;
+        ctx.strokeStyle = snakeBorderColor;
+        ctx.fillRect(unit.x, unit.y, uniteSize, uniteSize);
+        ctx.strokeRect(unit.x, unit.y, uniteSize, uniteSize);
+    });
 }
-function changeDirection() { }
-function checkGameOver() { }
-function showGameOver() { }
-function resetGame() { }
+
+function drawFood() {
+    let foodXPosition = Math.floor(Math.random() * canvasWidth);
+    let foodYPosition = Math.floor(Math.random() * canvasHeight);
+    ctx.fillStyle = foodColor;
+    ctx.fillRect(foodXPosition, foodYPosition, uniteSize, uniteSize);
+}
+
+function startGame() {
+
+    setTimeout(() => {
+        clearScreen();
+        drawFood();
+        drawSnake();
+        moveSnake();
+        requestAnimationFrame(startGame);
+    }, 500);
+}
+startGame();
+
+function moveSnake() {
+    const head = { x: snake[0].x + uniteSize, y: snake[0].y };
+    snake.unshift(head);
+    snake.pop();
+}
+
+function snakeController(){
+    
+}
+function clearScreen() {
+    ctx.fillStyle = gameCanvasBgColor;
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+}
